9befc1be86a32040bd31fc9834158a6683cc4b06
praveenkumarcs <praveenkumarcs@missionrnd.com>
2016-04-02 20:57:47 +0530

    Submit Commit

--- src/ArraySequences.cpp
+++ src/ArraySequences.cpp
@@ -1,5 +1,5 @@
 /*
-Given an array which has two arithmetic sequences and 
+Given an array which has two arithmetic sequences and
 one geometric sequence (in any order).
 Copy indexes where each sequence starts and ends into an Array and return it.
 
@@ -9,20 +9,20 @@ Ex : 1, 3, 5, 7, 2, 4, 8, 16 18 20, output is [0, 3, 7, 9, 4, 7]
 Ex : 1 ,2, 4, 8, 12, 16, 21, 26     output is [2, 5, 5, 7, 0, 3]
 Ex : 10 15 20 30 50 70 100 200 400 800  output is [0, 2, 3, 5, 6, 9]
 
-The final_arr[0], final_arr[1] values in final array  
+The final_arr[0], final_arr[1] values in final array
 will be start and end indexes of First A.P in the Original Array
 
-The final_arr[2], final_arr[3] values in final array  
-will be start and end indexes of Second A.P 
+The final_arr[2], final_arr[3] values in final array
+will be start and end indexes of Second A.P
 
-The final_arr[4], final_arr[5] values in final array  
+The final_arr[4], final_arr[5] values in final array
 will be start and end indexes of First  G.P (Only)
 
 Input: Array and Length of Array
 Output : New Array which has 6 integers , which denote the (start,end) indexes .
 
 Note : All elements in the array will be part of atleast 1 sequence .
-Two A.P cannot start at same index . 
+Two A.P cannot start at same index .
 
 Difficulty : Medium
 */
@@ -32,6 +32,79 @@ Difficulty : Medium
 #include <math.h>
 
 int * find_sequences(int *arr, int len){
+	int i, j = 0, commdiff, *ptr = NULL, k = 0;
+	int start, end;
 	//Return final array which has 6indexes [AP1_S,AP1_E,AP2_S,AP2_E,GP1_S,GP2_E]
-	return NULL;
+	ptr = (int *)malloc(6 * sizeof(int));
+
+
+
+	if (arr == NULL || len <= 0)
+		return NULL;
+
+	start = 1;
+	end = 1;
+	while (start <= len - 1){
+		end = start;
+		while (end < len - 1){
+			if (arr[end] - arr[end - 1] == arr[end + 1] - arr[end]){
+
+				end++;
+				continue;
+
+			}
+			else
+				break;
+
+		}
+
+		if (end - start >= 1){
+			ptr[k++] = start - 1;
+			ptr[k++] = end;
+			start += end;
+
+		}
+		else
+			start++;
+
+
+	}
+
+	start = 1;
+	end = 1;
+	while (start <= len - 1){
+		end = start;
+		while (end < len - 1){
+			if ((arr[end] / (float)arr[end - 1]) == ((float)arr[end + 1] / arr[end])){
+
+				end++;
+				continue;
+
+			}
+			else
+				break;
+
+		}
+
+		if (end - start >= 1){
+			ptr[k++] = start - 1;
+			ptr[k++] = end;
+			start += end;
+		}
+		else
+			start++;
+
+
+	}
+
+
+
+
+
+	return ptr;
+
 }
+
+
+
+
